01ff474bb7d92f5f842b071bc719e0d7589ab42d
Leonardo Cecchi <leonardo.cecchi@gmail.com>
2024-08-15 08:44:23 +0200

    Wip

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Main.Utf8 qualified as Utf8
+import Control.Concurrent
+import Control.Exception
+import Database.PostgreSQL.Simple qualified as D
 
 {- |
  Main entry point.
@@ -10,5 +12,21 @@ import Main.Utf8 qualified as Utf8
 main :: IO ()
 main = do
   -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  Utf8.withUtf8 $ do
-    putTextLn "Hello 🌎"
+  connectionCycle
+
+connectionTest :: IO ()
+connectionTest = do
+  putStrLn "Creating a connection to PG"
+  _ <- D.connectPostgreSQL "connect_timeout=2"
+  putStrLn "Waiting for a bit"
+  threadDelay 1000000
+
+connectionCycle :: IO ()
+connectionCycle = do
+  result <- try connectionTest
+  case result of
+    Left (err :: SomeException) -> do
+      putStrLn $ "error " ++ show err
+      threadDelay 1000000
+    Right _ -> putStrLn "Everything fine"
+  connectionCycle
